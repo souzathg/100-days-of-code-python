36a5a306dfedc0ae566bbef8e8f7a8534ea7a121
Thiago Costa de Souza <thgsouza@outlook.com>
2022-08-23 14:03:22 -0300

    day-16-oop-coffee-machine

--- day-16-oop-coffee-machine/main.py
+++ day-16-oop-coffee-machine/main.py
@@ -9,23 +9,14 @@ is_on = True
 
 while is_on:
     choice = input(f"What would you like? ({menu.get_items()}): ")
-
     if choice == "off":
         is_on = False
-        continue
     elif choice == "report":
         coffee_maker.report()
         money_machine.report()
-        continue
     else:
         drink = menu.find_drink(choice)
         if drink is None:
             continue
-
-    if not coffee_maker.is_resource_sufficient(drink):
-        continue
-
-    if not money_machine.make_payment(drink.cost):
-        continue
-
-    coffee_maker.make_coffee(drink)
+        if not coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
